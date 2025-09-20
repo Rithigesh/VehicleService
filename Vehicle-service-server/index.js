84570dd0bfafc2268afa3c04480baec01cd8506c
Rithigesh <158757694+Rithigesh@users.noreply.github.com>
2025-09-20 10:57:30 +0530

    password changed

--- Vehicle-service-server/index.js
+++ Vehicle-service-server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({ 
   host: 'localhost', 
   user: 'root',  
-  password: 'enter the password', 
+  password: '2004', 
   database: 'sb'
 });
 
